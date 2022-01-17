5528196f2640ca779ad8e35b2694f4a0ccd7965c
Matheus <stvgamer74@gmail.com>
2022-01-17 15:29:57 -0300

    R03: feito!

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,14 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (name, age) => {
+  if (name == null || age == null) {
+    return undefined;
+  }
+  return `Oi, meu nome é ${name}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+};
 
 module.exports = vqv;
