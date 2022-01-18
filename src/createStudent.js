8efe9e8e5e3a2cdb639484119266eb1f15828ffa
Matheus <stvgamer74@gmail.com>
2022-01-18 13:57:03 -0300

    R05: feito!

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,8 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const goodPerson = () => 'Eita pessoa boa!';
+
+const createStudent = (nome) => ({ name: nome, feedback: goodPerson });
 
 module.exports = createStudent;
